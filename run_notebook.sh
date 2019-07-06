c12f403ff8fb14aa6ff13785f3ec27b0fd28df60
leandro <leandro.vonwerra@spoud.io>
2019-07-06 16:40:37 +0200

    chore: update installation and simplify input/outbook nb name

--- run_notebook.sh
+++ run_notebook.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 notebook_path="ltu-0.0-lstm.ipynb"
-notebook_out_path="/artifacts/ltu-0.0-lstm.ipynb"
+notebook_out_path="/artifacts/$notebook_path"
 
 export LC_ALL=C.UTF-8
 
-pip install papermill
-pip install pandas
-pip install scikit-learn
+pip -q install papermill
+pip -q install pandas
+pip -q install scikit-learn
+pip -q install gradient_statsd
 
 cd notebooks/baselines
 papermill $notebook_path $notebook_out_path --log-output
