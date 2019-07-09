31ef7de442f14667970a9b55d9ff6543c51f5785
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2019-07-08 17:14:52 -0700

    refactor: replace LSTM script generated from notebook with dedicated one

--- src/models/train_lstm.py
+++ src/models/train_lstm.py
@@ -6,7 +6,7 @@
     --name "lstm_test" \
     --container tensorflow/tensorflow:2.0.0a0-gpu-py3-jupyter \
     --machineType GPU+ \
-    --command "/paperspace/run_lstm.sh" \
+    --command "/paperspace/run_script.sh" \
     --ignoreFiles "data,env"
 """
 import os
