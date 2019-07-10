1ef603a7279226d255f3a0b70dc5ca172056f0cf
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2019-07-10 11:55:12 -0700

    feat: add CPU support for LSTM encoder / decoder

--- src/models/lstm.py
+++ src/models/lstm.py
@@ -3,9 +3,10 @@ This seq2seq implementation is largely copied from:
 https://keras.io/examples/lstm_seq2seq/
 """
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.layers import Input, Dense, LSTM
 from tensorflow.compat.v1.keras.layers import CuDNNLSTM
 import numpy as np
+import tensorflow as tf
 
 
 class LSTM_S2S:
@@ -17,7 +18,11 @@ class LSTM_S2S:
     def get_model(self):
         # Define an input sequence and process it.
         self.encoder_inputs = Input(shape=(None, self.num_encoder_tokens))
-        encoder = CuDNNLSTM(self.latent_dim, return_state=True)
+        # Use CuDNNLSTM if running on GPU
+        if tf.test.is_gpu_available():
+            encoder = CuDNNLSTM(self.latent_dim, return_state=True)
+        else:
+            encoder = LSTM(self.latent_dim, return_state=True)
         encoder_outputs, state_h, state_c = encoder(self.encoder_inputs)
         # We discard `encoder_outputs` and only keep the states.
         self.encoder_states = [state_h, state_c]
@@ -27,9 +32,14 @@ class LSTM_S2S:
         # We set up our decoder to return full output sequences,
         # and to return internal states as well. We don't use the
         # return states in the training model, but we will use them in inference.
-        self.decoder_lstm = CuDNNLSTM(
-            self.latent_dim, return_sequences=True, return_state=True
-        )
+        if tf.test.is_gpu_available():
+            self.decoder_lstm = CuDNNLSTM(
+                self.latent_dim, return_sequences=True, return_state=True
+            )
+        else:
+            self.decoder_lstm = LSTM(
+                self.latent_dim, return_sequences=True, return_state=True
+            )
         decoder_outputs, _, _ = self.decoder_lstm(
             self.decoder_inputs, initial_state=self.encoder_states
         )
