7d71957e461f4afd92b0fceb0b8992881164afb8
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2019-08-02 11:58:27 +0200

    feat: add exact match metric for encodings

--- notebooks/baselines/metrics.py
+++ notebooks/baselines/metrics.py
@@ -1,18 +1,37 @@
 import tensorflow as tf
 
+
 def exact_match_metric(y_true, y_pred):
-    
-    # get indices from vectors 
+
+    # get indices from vectors
     y_pred_argmax = tf.argmax(y_pred, axis=-1)
     y_true_argmax = tf.argmax(y_true, axis=-1)
-    
-    # get mask of rows with no entry
-    mask = tf.equal(tf.reduce_sum(y_true, axis=-1),0)
 
-    pred_match = tf.equal(y_pred_argmax, y_true_argmax)  
-    
+    # get mask of rows with no entry
+    mask = tf.equal(tf.reduce_sum(y_true, axis=-1), 0)
+
+    pred_match = tf.equal(y_pred_argmax, y_true_argmax)
+
     # if no label in y_true, then actual match doesn't matter --> equal=True
-    pred_match_fixed = tf.where(mask, tf.ones_like(pred_match, dtype=tf.bool), pred_match)
+    pred_match_fixed = tf.where(
+        mask, tf.ones_like(pred_match, dtype=tf.bool), pred_match
+    )
+
+    exact_match = tf.reduce_min(tf.cast(pred_match_fixed, tf.float32), axis=[1])
+    return tf.reduce_mean(exact_match)
+
+
+def exact_match_metric_index(y_true, y_pred):
+
+    # get mask of rows with no entry
+    mask = tf.equal(y_true, 0)
+
+    pred_match = tf.equal(y_pred, y_true)
+
+    # if no label in y_true, then actual match doesn't matter --> equal=True
+    pred_match_fixed = tf.where(
+        mask, tf.ones_like(pred_match, dtype=tf.bool), pred_match
+    )
 
     exact_match = tf.reduce_min(tf.cast(pred_match_fixed, tf.float32), axis=[1])
     return tf.reduce_mean(exact_match)
